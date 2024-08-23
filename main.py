aa7d281e09e84a88c522be3973d2c4dcd46955b7
Gustavo Henrique Farias <84677653+Gustavolobosolitario@users.noreply.github.com>
2024-08-23 12:30:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -890,7 +890,7 @@ def home_page():
             st.session_state.clear()
             st.experimental_get_query_params(pagina='home')
 
-        with st.container(borde=True):
+        with st.container(border=True):
             st.title('Reserva')
             col1, col2 = st.columns(2)
 
