ec25f6dd8a7fa714fb197b064b24de1c5704ec7f
Gustavo Henrique Farias <84677653+Gustavolobosolitario@users.noreply.github.com>
2024-09-09 11:17:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1047,17 +1047,18 @@ def logout():
 
 
 def home_page():
-    criar_tabelas()
     st.title("Bem-vindo ao Home!")
     st.write("Você está logado.")
+    criar_tabelas()
+    
     
     st.sidebar.image('logo.png', use_column_width=True)
 
-        # Renderiza a página com base no estado de login
-        if st.session_state.pagina == 'login':
-            login()
-        elif st.session_state.pagina == 'home':
-            home()
+# Renderiza a página com base no estado de login
+if st.session_state.pagina == 'login':
+    login()
+elif st.session_state.pagina == 'home':
+    home()
 
     if st.session_state.get('usuario_logado'):
         st.sidebar.header(f'Bem vindo, {st.session_state.nome_completo}')
