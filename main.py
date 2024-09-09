4366cefcd14f4ea6ed5e6e7bf42e67650cb86e60
Gustavo Henrique Farias <84677653+Gustavolobosolitario@users.noreply.github.com>
2024-09-09 11:16:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1054,10 +1054,10 @@ def home_page():
     st.sidebar.image('logo.png', use_column_width=True)
 
     # Renderiza a página com base no estado de login
-if st.session_state.pagina == 'login':
-    login()
-elif st.session_state.pagina == 'home':
-    home()
+    if st.session_state.pagina == 'login':
+        login()
+    elif st.session_state.pagina == 'home':
+        home()
 
     if st.session_state.get('usuario_logado'):
         st.sidebar.header(f'Bem vindo, {st.session_state.nome_completo}')
